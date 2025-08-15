4aeeaa2cf0da65657363c9d850ed094564912eae
Troy Krause <troykrause07@gmail.com>
2025-08-15 21:39:16 +0200

    Add Basic Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,17 @@
+const { validationResult } = require("express-validator");
+
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const generateToken = (userId) => jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
 exports.register = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Invalid input", errors: errors.array() });
+    }
+
     const { email, password } = req.body;
     try {
         const existing = await User.findOne({ email });
@@ -18,6 +26,12 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Invalid input", errors: errors.array() });
+    }
+
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
